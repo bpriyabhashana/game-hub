de2738851fdfbf7a4eafb493da86a1096608d0e8
bpriyabhashana <bpriyabhashana@gmail.com>
2023-10-18 21:38:15 +0530

    Build platform selector

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
+  baseURL: "",
   params: {
-    key: "9cbbf1a99d2648b0b13b3f8501c08d31",
+    key: "",
   },
 });
